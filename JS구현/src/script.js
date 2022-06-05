f345910c821bbf71f4244e2d1eb3dce517cc6aae
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-06-06 01:58:23 +0900

    Complete putting in cart

---
+++ JS구현/src/script.js
@@ -0,0 +1,159 @@
+//JSON setting
+var requestURL = ''; // JSON 데이터 받아올 url
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL); // requestURL로 보냄
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+    var superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+    }
+
+    // JSON에서 받아올 데이터 설정해줌
+    const drinks = [  
+      {
+        "name" : "Original_Cola",
+        "price" : 1000
+      }, 
+      {
+        "name" : "Violet_Cola",
+        "price" : 1000
+      }, 
+      {
+        "name" : "Yellow_Cola",
+        "price" : 1000
+      },
+      {
+        "name" : "Cool_Cola",
+        "price" : 1000
+      }, 
+      {
+        "name" : "Green_Cola",
+        "price" : 1000
+      }, 
+      {
+        "name" : "Orange_Cola",
+        "price" : 1000
+      }
+    ]
+    
+    // 변수
+    const colaCart = document.querySelector(".cola-cart");
+    const menu = document.querySelector(".cola-menu");
+    const nowMoney = document.querySelector(".now-money");
+
+    // drink 넣는 함수
+    const makeDrink = function(addThisCola, parent){
+      // const item = this.item;
+      // const parent = this.parent;
+      // 여기 이 친구들 들어가면 왜 안될까?
+
+      const liElement = document.createElement("li");
+      const buttonElement = document.createElement("button");
+      const imgElement = document.createElement("img");
+      const strongElement = document.createElement("strong");
+      const spanElement = document.createElement("span");
+  
+      imgElement.setAttribute("src", `./images/${addThisCola.name}.svg`);
+  
+      strongElement.innerText = `${addThisCola.name}`
+      strongElement.classList.add("cola-btn-name");
+  
+      spanElement.innerText = `${addThisCola.price}`;
+      spanElement.classList.add("cola-btn-price");
+  
+      buttonElement.classList.add("cola-button")
+      buttonElement.appendChild(imgElement);
+      buttonElement.appendChild(strongElement);
+      buttonElement.appendChild(spanElement);
+  
+      liElement.appendChild(buttonElement);
+      parent.appendChild(liElement);  
+    }
+    
+    // cola-cart용 drink 넣는 함수
+    const colaInCart = function(e, parent){
+      const count = 1;
+
+      const liElement = document.createElement("li");
+      liElement.classList.add("picked-cola")
+
+      const imgElement = document.createElement("img");
+      imgElement.setAttribute("src", `./images/${e.name}.svg`);
+      imgElement.classList.add("picked-cola-img");
+      liElement.appendChild(imgElement);
+
+      const strongElement = document.createElement("strong");
+      strongElement.innerText = `${e.name}`
+      strongElement.classList.add("picked-cola-name")
+      liElement.appendChild(strongElement);
+
+      const spanElement = document.createElement("span");
+      spanElement.classList.add("cola-quantity");
+      spanElement.innerText = count;
+      liElement.appendChild(spanElement);
+      
+      liElement.classList.add("ir");
+      liElement.classList.add(e.name);
+      parent.appendChild(liElement);
+    }
+
+    // 기능 넣는 클래스
+    class Manager {
+      constructor (){
+    
+      }
+      menuSetting(){
+        drinks.forEach((item) => {
+          makeDrink(item, menu);
+        })
+      }
+
+      picking () {
+        drinks.forEach((item) => {
+          colaInCart(item, colaCart);
+        });
+        const menuColas = menu.querySelectorAll("li");
+        const colasInCart = colaCart.querySelectorAll("li");
+
+        menuColas.forEach(e => {
+          e.addEventListener("click", e => {
+
+            const clickedColaName = e.target
+              .closest("button")
+              .querySelector("strong")
+              .innerText;
+
+            const cartColaClicked = colaCart.querySelector(`.${clickedColaName}`);
+
+            if(cartColaClicked.classList.contains("ir")){
+              cartColaClicked.classList.remove("ir");
+            } else {
+              cartColaClicked.querySelector("span").innerText++;
+            }
+          });
+
+        })
+      }
+
+      putMoney () {
+        
+      }
+
+      changing() {
+    
+      }
+    }
+
+    // 인스턴스 생성
+    const manager = new Manager();
+
+    // 기본 세팅
+    manager.menuSetting();
+    manager.picking();
+
+    // cola cart로
+    const colaButton = document.querySelectorAll(".cola-button");
+
---
+++ src/script.js
@@ -0,0 +1,25 @@
+//돈 넣으면 추가되는 함수
+
+const colaCart = document.querySelector("picked-cola");
+
+// 기능구현
+class Manager {
+  constructor (){
+
+  }
+  picking () {
+    const colaButton = document.querySelector("cola-button");
+    colaButton.addEventListener("click", (e) => {
+      const myEl = document.createElement("li");
+
+    })
+  }
+  putMoney () {
+
+  }
+  changing() {
+
+  }
+}
+
+// module 넣는 함수를 만들까?
